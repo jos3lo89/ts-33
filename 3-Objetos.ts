84c64f96a439fcacf8c3215d969f4cb0de85480f
jos3lo89 <gothangel21@hotmail.com>
2024-07-31 11:57:41 -0500

    edit files

--- 3-Objetos.ts
+++ 3-Objetos.ts
@@ -1,18 +1,38 @@
-type Hero = {
+type HeroPowerScale =
+  | "local"
+  | "planetary"
+  | "galactic"
+  | "universal"
+  | "multiuniversal";
+
+type HeroId = `${string}-${string}-${string}-${string}-${string}`;
+type HeroBasicinfo = {
   name: string;
   age: number;
 };
+type HeroProperties = {
+  id?: HeroId;
+
+  isActive?: boolean;
+  powerScale?: HeroPowerScale;
+};
+
+type Hero = HeroBasicinfo & HeroProperties;
 
 let hero: Hero = {
   name: "Thor",
   age: 1500,
 };
 
-function createHero(name: string, age: number): Hero {
+function createHero(hero: Hero): Hero {
+  const { name, age } = hero;
   return {
+    id: crypto.randomUUID(),
     name,
     age,
+    isActive: true,
   };
 }
 
-let thor = createHero("Thor", 1500);
+let thor = Object.freeze(createHero({ name: "Thor", age: 1500 }));
+console.log(thor);
